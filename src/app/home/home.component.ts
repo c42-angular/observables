83530c168a1f55234f05a6d649f18e1e0f4618a2
Arunkumar Ramachandriya <arun@Arunkumars-MacBook-Pro.local>
2018-01-14 07:50:24 +0000

    Sample observables and unsubscribe pattern

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,15 +1,67 @@
 import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs/Observable';
+import 'rxjs/Rx';
+import { Observer } from 'rxjs/Observer';
+import { Subscription } from 'rxjs/Subscription';
+import { OnDestroy } from '@angular/core/src/metadata/lifecycle_hooks';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
-export class HomeComponent implements OnInit {
+export class HomeComponent implements OnInit, OnDestroy {
+  numbersObsSubscription: Subscription;
+  customObsSubscription: Subscription;
 
   constructor() { }
 
   ngOnInit() {
+    const myNumbers = Observable.interval(1000);
+    this.numbersObsSubscription = myNumbers.subscribe(
+      (number: number) => {
+        console.log(number);
+      }
+    );
+
+    // kick off an observable
+    const myObservable = Observable.create((observer: Observer<string>) => {
+      setTimeout(() => {
+        observer.next('first package');
+      }, 2000);
+
+      setTimeout(() => {
+        observer.next('second package');
+      }, 4000);
+
+      setTimeout(() => {
+        observer.complete();
+      }, 6000);
+      setTimeout(() => {
+        observer.error('error package');
+      }, 8000);
+    });
+
+    // subscribe to it
+    this.customObsSubscription = myObservable.subscribe(
+      // data handler
+      (data: string) => {
+        console.log(data);
+      },
+      // error handler
+      (error: string) => {
+        console.log(error);
+      },
+      // completion handler
+      () => {
+        console.log('completed');
+      },
+    );
+  }
+
+  ngOnDestroy(): void {
+    this.numbersObsSubscription.unsubscribe();
+    this.customObsSubscription.unsubscribe();
   }
 
 }
